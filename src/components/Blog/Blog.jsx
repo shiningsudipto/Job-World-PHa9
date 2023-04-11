ad792710eb1c35177620b24fbdf2030936add18e
Sudipto Das <shiningsudipto@gmail.com>
2023-04-11 23:16:02 +0600

    Question answer added to the blog page

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -4,6 +4,19 @@ const Blog = () => {
     return (
         <div>
             <h3>This is Blog page</h3>
+            <h3 className='text-2xl font-semibold mt-8 mb-4'>When should you use context API?</h3>
+            <p>When I need to pass data down multiple levels of the component tree. In situations where data needs to be accessed by components that are several levels deep in the hierarchy, using Context API can simplify the process of passing data down through props.</p>
+            <h3 className='text-2xl font-semibold mt-8 mb-4'>What is custom hook?</h3>
+            <p>A custom hook is a function that uses the React Hooks API to implement reusable logic that can be shared across multiple components in a React application.</p>
+            <h3 className='text-2xl font-semibold mt-8 mb-4'>What is the purpose of useRef</h3>
+            <p>The useRef Hook allows to persist values between renders.
+                It can be used to store a mutable value that does not cause a re-render when updated.
+                It can be used to access a DOM element directly.</p>
+            <h3 className='text-2xl font-semibold mt-8 mb-4'>What is the purpose of useMemo</h3>
+            <p>The React useMemo Hook returns a memoized value.</p>
+            <p>This can improve performance.</p>
+            <p>The useMemo Hook can be used to keep expensive, resource intensive functions from needlessly running.</p>
+            <p>We can wrap the expensive function call with useMemo.</p>
         </div>
     );
 };
