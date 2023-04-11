1c0697ef7e46d67449f865ac4983831a3d6d914f
Sudipto Das <shiningsudipto@gmail.com>
2023-04-11 20:34:05 +0600

    filter done

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -14,11 +14,26 @@ const AppliedJobs = () => {
         }
     }, []);
 
-    console.log(jobDetails);
+    const handleAppliedJobsByRemote = (filterC) => {
+        const existingData = localStorage.getItem("jobDetails");
+        if (existingData) {
+            const parsedData = JSON.parse(existingData);
+            const filteredData = parsedData.filter(job => job.remote_or_onsite === filterC);
+            setJobDetails(filteredData);
+        }
+    }
 
     return (
         <div className='container mx-auto'>
-
+            <div className='text-end'>
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn me-8">Click</label>
+                    <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box">
+                        <li><button onClick={() => handleAppliedJobsByRemote('Onsite')}>Onsite</button></li>
+                        <li><button onClick={() => handleAppliedJobsByRemote('Remote')}>Remote</button></li>
+                    </ul>
+                </div>
+            </div>
             <div>
                 {
                     jobDetails.map(myJob => <MyJob
